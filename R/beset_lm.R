da7d10c9656f3acd0319c6822d20a872cca4c15c
Jason Shumake <shumake@utexas.edu>
2016-08-11 13:44:53 -0500

    Remove hidden dependency on dplyr.

--- R/beset_lm.R
+++ R/beset_lm.R
@@ -174,8 +174,8 @@ beset_lm <- function(form, train_data, test_data = NULL, n_folds = 10,
                        R2_test_SE = R2_test_SE)
     if(n == 1) R2 <- temp else R2 <- rbind(R2, temp)
   }
-  R2 <- group_by(R2, n_preds) %>%
-    summarize_each(funs(mean))
+  R2 <- dplyr::group_by(R2, n_preds)
+  R2 <- dplyr::summarize_each(R2, funs(mean))
   max_R2 <- max(R2$R2_cv)
   if(max_R2 <= 0){
     best_model <- lm(paste(response, "~ 1"), data = mf)
