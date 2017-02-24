8d79639cd283c18c6459d1008f5d1f28e9cef361
Jason Shumake <shumake@utexas.edu>
2017-02-24 17:36:35 -0600

    Revise plot method to plot different prediction metrics.

--- R/plot.beset_glm.R
+++ R/plot.beset_glm.R
@@ -20,28 +20,45 @@
 #' @import ggplot2
 #'
 #' @export
-plot.beset_glm <- function(object, SE = TRUE, title = ""){
-  data <- object$best_subsets
-  data$CE_cv_lower <- with(data, cv_CE - cv_CE_SE)
-  data$CE_cv_upper <- with(data, cv_CE + cv_CE_SE)
+plot.beset_glm <- function(object, metric = "MCE", se = TRUE, title = ""){
+  train <- dplyr::select(object$fit_stats, n_pred, form, starts_with(metric))
+  names(train)[3] <- "train"
+  xval <- dplyr::select(object$xval_stats, n_pred, form, starts_with(metric))
+  names(xval)[3:4] <- c("cv", "cv_se")
+  test <- try(dplyr::select(object$test_stats, n_pred, form,
+                            starts_with(metric)), silent = TRUE)
+  if(class(test) == "try-error"){
+    test <- NULL
+  } else {
+    names(test)[3] <- "test"
+  }
+  train <- dplyr::filter(train, form %in% xval$form)
+  data <- dplyr::left_join(train, xval, by = c("n_pred", "form"))
+  if(!is.null(test)) data <- dplyr::left_join(data, test)
+  data$cv_lower <- with(data, cv - cv_se)
+  data$cv_upper <- with(data, cv + cv_se)
   xmax <- max(data$n_pred)
   color_legend <- c("Train" = "grey", "CV" = "red", "Test" = "blue")
+  y_lab <- switch(metric,
+                  MCE = ylab("Mean Cross Entropy (-loglik / N)"),
+                  MSE = ylab("Mean Squared Error"),
+                  R2 = ylab(bquote(~R^2)))
   p <- ggplot(data = data) +
     theme_bw() +
     ggtitle(title) +
     xlab("Number of Predictors") +
-    ylab("Cross-Entropy Error") +
+    y_lab +
     scale_x_continuous(breaks = 0:xmax) +
     scale_color_manual(name = "", values = color_legend) +
-    geom_line(aes(x = n_pred, y = train_CE, color = "Train")) +
-    geom_line(aes(x = n_pred, y = cv_CE, color = "CV"))
-  if(SE){
+    geom_line(aes(x = n_pred, y = train, color = "Train")) +
+    geom_line(aes(x = n_pred, y = cv, color = "CV"))
+  if(se){
     p <- p +
-      geom_errorbar(aes(x = n_pred, ymin = CE_cv_lower, ymax = CE_cv_upper),
+      geom_errorbar(aes(x = n_pred, ymin = cv_lower, ymax = cv_upper),
                     width = 0.2, color = "red")
   }
-  if(!any(is.na(data$test_CE))){
+  if(!is.null(test)){
     p <- p + geom_line(aes(x = n_pred, y = test_CE, color = "Test"))
   }
-  p
+  print(p)
 }
