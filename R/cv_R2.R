dc15c68926ef3bc90fc6ae9223c4ffcf00910da8
Jason Shumake <shumake@utexas.edu>
2017-03-02 10:34:21 -0600

    Add error handling to cv_R2 in the event there is a problem with one or more of the folds

--- R/cv_R2.R
+++ R/cv_R2.R
@@ -5,8 +5,10 @@
 #'
 #' @param n_cores Integer value indicating the number of workers to run in
 #' parallel during subset search and cross-validation. By default, this will
-#' be set to 2. You may wish to increase this depending on your hardware and OS.
-#' See \code{\link[parallel]{parallel-package}} for more information.
+#' be set to 2. To determine the theoretical maximum number of cores you have
+#' available, see \code{\link[parallel]{detectCores}}, but note that the actual
+#' number of cores available may be less. See
+#' \code{\link[parallel]{parallel-package}} for more information.
 #'
 #' @param n_folds Integer indicating the number of folds to use for
 #' cross-validation.
@@ -19,23 +21,24 @@
 #'
 #' @export
 cv_r2 <- function(object, n_cores = 2, n_folds = 10, n_repeats = 10, seed = 42){
-  model_type <- class(object)[1]
   y <- object$model[,1]
   set.seed(seed)
   fold_ids <- caret::createMultiFolds(y, k = n_folds, times = n_repeats)
   cl <- parallel::makeCluster(n_cores)
-  r2 <- parallel::parSapply(cl, fold_ids, function(x, object){
-    model <- switch(
-      model_type,
-      lm = lm(object$terms, data = object$model[x,]),
-      glm = glm(object$terms, data = object$model[x,],
+  r2 <- parallel::parSapply(cl, fold_ids, function(i, object){
+    fit <- switch(
+      class(object)[1],
+      lm = lm(object$terms, data = object$model[i,]),
+      glm = glm(object$terms, data = object$model[i,],
                 family = object$family$family),
-      negbin = MASS::glm.nb(object$terms, data = object$model[x,]),
+      negbin = MASS::glm.nb(object$terms, data = object$model[i,]),
       zeroinfl = pscl::zeroinfl(object$terms$full,
-                                data = object$model[x,],
+                                data = object$model[i,],
                                 dist = object$dist)
     )
-    beset::prediction_metrics(model, object$model[-x,])$R_squared
+    stats <- try(predict_metrics(fit, test_data = object$model[-i,]),
+                 silent = TRUE)
+    if(class(stats) == "prediction_metrics") stats$R_squared else NA
   }, object = object)
   boot_r2 <- boot::boot(r2, function(x, i) median(x[i], na.rm = TRUE), 1000,
                         parallel = "snow", cl = cl)
