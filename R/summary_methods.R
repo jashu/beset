3de2e9847d671fafcd9a69a1c318e473a4eef629
Jason Shumake <shumake@utexas.edu>
2017-03-10 05:32:57 -0600

    Change names to lowercase

--- R/summary_methods.R
+++ R/summary_methods.R
@@ -6,7 +6,7 @@
 #' \code{"beset_zeroinfl"}, or \code{"beset_elnet"}
 #'
 #' @param metric Prediction metric on which to based model selection. Can be one
-#' of "MCE" (mean cross entropy), "MSE" (mean squared error), or "R2"
+#' of "mce" (mean cross entropy), "mse" (mean squared error), or "r2"
 #' R-squared).
 #'
 #' @param n_pred Optional: number of predictors that the best model should
@@ -29,35 +29,35 @@ NULL
 
 #' @export
 #' @rdname summary.beset
-summary.beset_elnet <- function(object, metric = "MCE", oneSE = TRUE,
+summary.beset_elnet <- function(object, metric = "mce", oneSE = TRUE,
                                 n_cores = 2, ...){
-  metric <- match.arg(metric, c("MCE", "MSE", "R2"))
+  metric <- match.arg(metric, c("mce", "mse", "r2"))
   if(is.na(metric)) stop("invalid 'metric' argument")
   best_metric <- switch(
     metric,
-    MCE = min(object$stats$xval$MCE, na.rm = T)[1],
-    MSE = min(object$stats$xval$MSE, na.rm = T)[1],
-    R2 = max(object$stats$xval$R2, na.rm = T)[1]
+    mce = min(object$stats$xval$mce, na.rm = T)[1],
+    mse = min(object$stats$xval$mse, na.rm = T)[1],
+    r2 = max(object$stats$xval$r2, na.rm = T)[1]
   )
   best_idx <- switch(
     metric,
-    MCE = which.min(object$stats$xval$MCE),
-    MSE = which.min(object$stats$xval$MSE),
-    R2 = which.max(object$stats$xval$R2)
+    mce = which.min(object$stats$xval$mce),
+    mse = which.min(object$stats$xval$mse),
+    r2 = which.max(object$stats$xval$r2)
   )
   best_alpha <- object$stats$xval$alpha[best_idx]
   best_lambda <- object$stats$xval$lambda[best_idx]
   if(oneSE){
     boundary <- switch(
       metric,
-      MCE = best_metric + object$stats$xval$MCE_SE[best_idx],
-      MSE = best_metric + object$stats$xval$MSE_SE[best_idx],
-      R2 = best_metric - object$stats$xval$R2_SE[best_idx])
+      mce = best_metric + object$stats$xval$mce_SE[best_idx],
+      mse = best_metric + object$stats$xval$mse_SE[best_idx],
+      r2 = best_metric - object$stats$xval$r2_SE[best_idx])
     best_1SE <- switch(
       metric,
-      MCE = object$stats$xval[object$stats$xval$MCE < boundary,],
-      MSE = object$stats$xval[object$stats$xval$MSE < boundary,],
-      R2 = object$stats$xval[object$stats$xval$R2 > boundary,]
+      mce = object$stats$xval[object$stats$xval$mce < boundary,],
+      mse = object$stats$xval[object$stats$xval$mse < boundary,],
+      r2 = object$stats$xval[object$stats$xval$r2 > boundary,]
     )
     best_alpha <- max(best_1SE$alpha)
     best_1SE <- dplyr::filter(best_1SE, alpha == best_alpha)
@@ -66,133 +66,134 @@ summary.beset_elnet <- function(object, metric = "MCE", oneSE = TRUE,
                         object$stats$fit$lambda == best_lambda)
   }
   best_model <- object$model_fits[[as.character(best_alpha)]]
-  coefs <- coef(best_model, s = best_lambda)
-  var_imp <- dplyr::data_frame(variable = rownames(coefs)[-1], coef = 0)
-  var_imp$coef[coefs@i[-1]] <- coefs@x[-1]
-  var_imp <- dplyr::arrange(var_imp, dplyr::desc(abs(coef)))
-  R2 <- object$stats$fit$R2[best_idx]
-  R2_test <- object$stats$test$R2[best_idx]
+  coefs <- stats::coef(best_model, s = best_lambda)
+  var_imp <- dplyr::data_frame(variable = rownames(coefs)[-1], stand.coef = 0)
+  var_imp$stand.coef[coefs@i[-1]] <- coefs@x[-1]
+  var_imp <- dplyr::arrange(var_imp, dplyr::desc(abs(stand.coef)))
+  r2 <- object$stats$fit$r2[best_idx]
+  r2_test <- object$stats$test$r2[best_idx]
   x <- object$model_params$x
   y <- object$model_params$y
   family <- object$model_params$family
-  cv_R2 <- sapply(object$xval_params$fold_ids, function(i){
-    fit <- update(best_model, x = x[i,], y = y[i], family = family,
+  cv_r2 <- sapply(object$xval_params$fold_ids, function(i){
+    fit <- stats::update(best_model, x = x[i,], y = y[i], family = family,
                   alpha = best_alpha)
-    y_hat <- predict(fit, x[-i,], best_lambda, "response")
+    y_hat <- stats::predict(fit, x[-i,], best_lambda, "response")
     predict_metrics_(y[-i], y_hat, family)$R_squared
   })
-  R2_boot <- boot::boot(cv_R2, function(x,i) median(x[i]), 1000)
-  boot_CI <- boot::boot.ci(R2_boot, type = "bca")
-  R2_cv <- structure(
-    list(cv_R2 = median(cv_R2, na.rm = TRUE), `95% CI` = boot_CI$bca[4:5],
-         R2 = cv_R2), class = "cv_R2")
+  r2_boot <- boot::boot(cv_r2, function(x,i) mean(x[i]), 1000)
+  boot_CI <- boot::boot.ci(r2_boot, type = "bca")
+  r2_cv <- structure(
+    list(cv_r2 = mean(cv_r2, na.rm = TRUE), `95% CI` = boot_CI$bca[4:5],
+         r2 = cv_r2), class = "cv_r2")
 
   structure(list(best = best_model, best_alpha = best_alpha,
                  best_lambda = best_lambda, var_imp = var_imp,
-                 R2 = R2, R2_cv = R2_cv, R2_test = R2_test),
+                 r2 = r2, r2_cv = r2_cv, r2_test = r2_test),
             class = "summary_beset_elnet")
 }
 
 #' @export
 #' @rdname summary.beset
-summary.beset_glm <- function(object, metric = "MCE", n_pred = NULL,
+summary.beset_glm <- function(object, metric = NULL, n_pred = NULL,
                               oneSE = TRUE, n_cores = 2, ...){
-  metric <- tryCatch(match.arg(metric, c("AIC", "MCE", "MSE", "R2")),
+  if(is.null(metric)){
+    metric <- if(object$best_aic$family$family == "gaussian") "mse" else "mce"
+  }
+  metric <- tryCatch(match.arg(metric, c("aic", "mae", "mce", "mse", "r2")),
                      error = function(c){
                        c$message <- gsub("arg", "metric", c$message)
                        c$call <- NULL
                        stop(c)
                      })
-  best_model <- object$best_AIC
+  best_model <- object$best_aic
   best_form <- object$stats$fit$form[1]
   if(!is.null(n_pred)){
     best_form <- object$stats$cv$form[object$stats$cv$n_pred == n_pred]
     best_model <- if(class(best_model)[1] == "negbin"){
-      update(best_model, best_form, data = object$model_data)
+      stats::update(best_model, best_form, data = object$model_data)
     } else {
-      update(best_model, best_form, family = best_model$family,
+      stats::update(best_model, best_form, family = best_model$family,
              data = object$model_data)
     }
   }
-  if(metric != "AIC" && is.null(n_pred)){
+  if(metric != "aic" && is.null(n_pred)){
     best_metric <- switch(
       metric,
-      MCE = min(object$stats$cv$MCE, na.rm = T)[1],
-      MSE = min(object$stats$cv$MSE, na.rm = T)[1],
-      R2 = max(object$stats$cv$R2, na.rm = T)[1]
+      mae = min(object$stats$cv$mae, na.rm = T)[1],
+      mce = min(object$stats$cv$mce, na.rm = T)[1],
+      mse = min(object$stats$cv$mse, na.rm = T)[1],
+      r2 = max(object$stats$cv$r2, na.rm = T)[1]
     )
     best_idx <- switch(
       metric,
-      MCE = which.min(object$stats$cv$MCE),
-      MSE = which.min(object$stats$cv$MSE),
-      R2 = which.max(object$stats$cv$R2)
+      mae = which.min(object$stats$cv$mae),
+      mce = which.min(object$stats$cv$mce),
+      mse = which.min(object$stats$cv$mse),
+      r2 = which.max(object$stats$cv$r2)
     )
     best_form <- object$stats$cv$form[best_idx]
     if(oneSE){
       boundary <- switch(
         metric,
-        MCE = best_metric + object$stats$cv$MCE_SE[best_idx],
-        MSE = best_metric + object$stats$cv$MSE_SE[best_idx],
-        R2 = best_metric - object$stats$cv$R2_SE[best_idx])
+        mae = best_metric + object$stats$cv$mae_SE[best_idx],
+        mce = best_metric + object$stats$cv$mce_SE[best_idx],
+        mse = best_metric + object$stats$cv$mse_SE[best_idx],
+        r2 = best_metric - object$stats$cv$r2_SE[best_idx])
       best_1SE <- switch(
         metric,
-        MCE = object$stats$cv[object$stats$cv$MCE < boundary,],
-        MSE = object$stats$cv[object$stats$cv$MSE < boundary,],
-        R2 = object$stats$cv[object$stats$cv$R2 > boundary,]
+        mae = object$stats$cv[object$stats$cv$mae < boundary,],
+        mce = object$stats$cv[object$stats$cv$mce < boundary,],
+        mse = object$stats$cv[object$stats$cv$mse < boundary,],
+        r2 = object$stats$cv[object$stats$cv$r2 > boundary,]
       )
       best_form <- best_1SE$form[which.min(best_1SE$n_pred)]
     }
     best_model <- if(class(best_model)[1] == "negbin"){
-      update(best_model, best_form, data = object$model_data)
+      stats::update(best_model, best_form, data = object$model_data)
     } else {
-      update(best_model, best_form, family = best_model$family,
+      stats::update(best_model, best_form, family = best_model$family,
              data = object$model_data)
     }
   }
   p <- object$stats$fit$n_pred[object$stats$fit$form == best_form]
-  loglik <- logLik(best_model)
+  loglik <- stats::logLik(best_model)
   best <- c(summary(best_model), loglik = loglik,
             loglik_df = attr(loglik, "df"))
-  R2 <- object$stats$fit$R2[object$stats$fit$form == best_form]
-  R2_test <- object$stats$test$R2[object$stats$test$form == best_form]
-  R2_cv <- do.call("cv_r2",
+  r2 <- object$stats$fit$r2[object$stats$fit$form == best_form]
+  r2_test <- object$stats$test$r2[object$stats$test$form == best_form]
+  r2_cv <- do.call("cv_r2",
                    args = c(list(object = best_model, n_cores = n_cores),
                             object$cv_params))
   near_equals <- dplyr::filter(object$stats$fit, n_pred == p)
-  if(metric == "AIC") metric <- "MCE"
-  near_equals <- dplyr::select(near_equals, form,
-                               metric = dplyr::starts_with(metric))
-  if(metric == "MCE") near_equals$metric <- exp(-near_equals$metric)
-  best_metric <- if(metric == "MSE") min(near_equals$metric, na.rm = T)[1] else
-    max(near_equals$metric, na.rm = T)[1]
-  boundary <- c(best_metric - .01*best_metric, best_metric + .01*best_metric)
-  near_best <- near_equals$form[dplyr::between(
-    near_equals$metric, boundary[1], boundary[2])]
+  best_metric <- max(near_equals$r2, na.rm = T)[1]
+  boundary <- c(best_metric - .01)
+  near_best <- near_equals$form[near_equals$r2 > boundary]
   near_best <- near_best[near_best != best_form]
 
   structure(list(best = best, best_form = best_form, near_best = near_best,
-                 R2 = R2, R2_cv = R2_cv, R2_test = R2_test),
+                 r2 = r2, r2_cv = r2_cv, r2_test = r2_test),
             class = "summary_beset_glm")
 }
 
 #' @export
 #' @rdname summary.beset
-summary.beset_zeroinfl <- function(object, metric = "MCE", n_count_pred = NULL,
+summary.beset_zeroinfl <- function(object, metric = "mce", n_count_pred = NULL,
                                    n_zero_pred = NULL, oneSE = TRUE,
                                    n_cores = 2, ...){
-  metric <- tryCatch(match.arg(metric, c("AIC", "MCE", "MSE", "R2")),
+  metric <- tryCatch(match.arg(metric, c("aic", "mae", "mce", "mse", "r2")),
                      error = function(c){
                        c$message <- gsub("arg", "metric", c$message)
                        c$call <- NULL
                        stop(c)
                      })
-  best_model <- object$best_AIC
+  best_model <- object$best_aic
   best_form <- object$stats$fit$form[1]
   if(!is.null(n_count_pred) && !is.null(n_zero_pred)){
     best_idx <- object$stats$cv$n_count_pred == n_count_pred &
       object$stats$cv$n_zero_pred == n_zero_pred
     best_form <- object$stats$cv$form[best_idx]
-    best_model <- pscl::zeroinfl(formula = formula(best_form),
+    best_model <- pscl::zeroinfl(formula = stats::formula(best_form),
                                  data = object$model_data,
                                  weights = best_model$weights,
                                  offset = best_model$offset$count,
@@ -200,44 +201,49 @@ summary.beset_zeroinfl <- function(object, metric = "MCE", n_count_pred = NULL,
                                  link = best_model$link,
                                  control = best_model$control)
   }
-  if(metric != "AIC" && is.null(n_zero_pred) && is.null(n_count_pred)){
+  if(metric != "aic" && is.null(n_zero_pred) && is.null(n_count_pred)){
     best_metric <- switch(
       metric,
-      MCE = min(object$stats$cv$MCE, na.rm = T)[1],
-      MSE = min(object$stats$cv$MSE, na.rm = T)[1],
-      R2 = max(object$stats$cv$R2, na.rm = T)[1]
+      mae = min(object$stats$cv$mae, na.rm = T)[1],
+      mce = min(object$stats$cv$mce, na.rm = T)[1],
+      mse = min(object$stats$cv$mse, na.rm = T)[1],
+      r2 = max(object$stats$cv$r2, na.rm = T)[1]
     )
     best_idx <- switch(
       metric,
-      MCE = which.min(object$stats$cv$MCE),
-      MSE = which.min(object$stats$cv$MSE),
-      R2 = which.max(object$stats$cv$R2)
+      mae = which.min(object$stats$cv$mae),
+      mce = which.min(object$stats$cv$mce),
+      mse = which.min(object$stats$cv$mse),
+      r2 = which.max(object$stats$cv$r2)
     )
     best_form <- object$stats$cv$form[best_idx]
     if(oneSE){
       boundary <- switch(
         metric,
-        MCE = best_metric + object$stats$cv$MCE_SE[best_idx],
-        MSE = best_metric + object$stats$cv$MSE_SE[best_idx],
-        R2 = best_metric - object$stats$cv$R2_SE[best_idx])
+        mae = best_metric + object$stats$cv$mae_SE[best_idx],
+        mce = best_metric + object$stats$cv$mce_SE[best_idx],
+        mse = best_metric + object$stats$cv$mse_SE[best_idx],
+        r2 = best_metric - object$stats$cv$r2_SE[best_idx])
       best_1SE <- switch(
         metric,
-        MCE = object$stats$cv[object$stats$cv$MCE < boundary,],
-        MSE = object$stats$cv[object$stats$cv$MSE < boundary,],
-        R2 = object$stats$cv[object$stats$cv$R2 > boundary,]
+        mae = object$stats$cv[object$stats$cv$mae < boundary,],
+        mce = object$stats$cv[object$stats$cv$mce < boundary,],
+        mse = object$stats$cv[object$stats$cv$mse < boundary,],
+        r2 = object$stats$cv[object$stats$cv$r2 > boundary,]
       )
       best_1SE <- dplyr::filter(best_1SE,
                                 (n_zero_pred + n_count_pred) ==
                                   min(n_zero_pred + n_count_pred))
       best_idx <- switch(
         metric,
-        MCE = which.min(best_1SE$MCE),
-        MSE = which.min(best_1SE$MSE),
-        R2 = which.max(best_1SE$R2)
+        mae = which.min(best_1SE$mae),
+        mce = which.min(best_1SE$mce),
+        mse = which.min(best_1SE$mse),
+        r2 = which.max(best_1SE$r2)
       )
       best_form <- best_1SE$form[best_idx]
     }
-    best_model <- pscl::zeroinfl(formula = formula(best_form),
+    best_model <- pscl::zeroinfl(formula = stats::formula(best_form),
                                  data = object$model_data,
                                  weights = best_model$weights,
                                  offset = best_model$offset$count,
@@ -247,28 +253,22 @@ summary.beset_zeroinfl <- function(object, metric = "MCE", n_count_pred = NULL,
   }
   pz <- object$stats$fit$n_zero_pred[object$stats$fit$form == best_form]
   pc <- object$stats$fit$n_count_pred[object$stats$fit$form == best_form]
-  loglik <- logLik(best_model)
+  loglik <- stats::logLik(best_model)
   best <- c(summary(best_model), loglik = loglik,
-            loglik_df = attr(loglik, "df"), aic = AIC(best_model))
-  R2 <- object$stats$fit$R2[object$stats$fit$form == best_form]
-  R2_test <- object$stats$test$R2[object$stats$test$form == best_form]
-  R2_cv <- do.call("cv_r2",
+            loglik_df = attr(loglik, "df"), aic = stats::AIC(best_model))
+  r2 <- object$stats$fit$r2[object$stats$fit$form == best_form]
+  r2_test <- object$stats$test$r2[object$stats$test$form == best_form]
+  r2_cv <- do.call("cv_r2",
                    args = c(list(object = best_model, n_cores = n_cores),
                             object$cv_params))
   near_equals <- dplyr::filter(object$stats$fit,
                                (n_zero_pred + n_count_pred) == (pc + pz))
-  if(metric == "AIC") metric <- "MCE"
-  near_equals <- dplyr::select(near_equals, form,
-                               metric = dplyr::starts_with(metric))
-  if(metric == "MCE") near_equals$metric <- exp(-near_equals$metric)
-  best_metric <- if(metric == "MSE") min(near_equals$metric, na.rm = T)[1] else
-    max(near_equals$metric, na.rm = T)[1]
-  boundary <- c(best_metric - .01*best_metric, best_metric + .01*best_metric)
-  near_best <- near_equals$form[dplyr::between(
-    near_equals$metric, boundary[1], boundary[2])]
+  best_metric <- max(near_equals$r2, na.rm = T)[1]
+  boundary <- best_metric - .01
+  near_best <- near_equals$form[near_equals$r2 > boundary]
   near_best <- near_best[near_best != best_form]
 
   structure(list(best = best, best_form = best_form, near_best = near_best,
-                 R2 = R2, R2_cv = R2_cv, R2_test = R2_test),
+                 r2 = r2, r2_cv = r2_cv, r2_test = r2_test),
             class = "summary_beset_zeroinfl")
 }
