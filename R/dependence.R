jashu/beset

commit 4c53361d66e4457b966fb592f8c1f62965a020a6

Change class inheritance structure of beset rf objects to be consistent with other beset objects

R/dependence.R
@@ -68,8 +68,14 @@ dependence.nested <- function(object, x = NULL, cond = NULL,
 #' @export
 dependence.beset <- function(
   object, x = NULL, cond = NULL, alpha = NULL, lambda = NULL, n_pred = NULL,
-  metric = "auto", oneSE = TRUE, x_lab = NULL, y_lab = NULL, ...
+  metric = "auto", oneSE = TRUE, x_lab = NULL, y_lab = NULL, ...,
+  parallel_type = NULL, n_cores = NULL, cl = NULL
 ){
+  if(inherits(x, "rf")){
+    return(dependence.beset_rf(object, x, cond, x_lab, y_lab, ...,
+      parallel_type = parallel_type, n_cores = n_cores, cl = cl
+    ))
+  }
   model_data <- list(...)
   if(is.null(object$data)){
     object$data <- model_data$data

commit a136582b8c2702cb9655f2565ef6e69c1ec70b1d

Fix bug in method dispatch for beset rf introduced by commit 4c53361d

R/dependence.R
@@ -71,7 +71,7 @@ dependence.beset <- function(
   metric = "auto", oneSE = TRUE, x_lab = NULL, y_lab = NULL, ...,
   parallel_type = NULL, n_cores = NULL, cl = NULL
 ){
-  if(inherits(x, "rf")){
+  if(inherits(object, "rf")){
     return(dependence.beset_rf(object, x, cond, x_lab, y_lab, ...,
       parallel_type = parallel_type, n_cores = n_cores, cl = cl
     ))